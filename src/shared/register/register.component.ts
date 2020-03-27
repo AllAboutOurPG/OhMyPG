24d0ac52e239bcd52f55ece79cd1ec0125ef23d4
Rajineesh Racharla <stylish.rajineesh@gmail.com>
2020-03-27 14:01:24 +0530

    Modified personal details page

--- src/shared/register/register.component.ts
+++ src/shared/register/register.component.ts
@@ -14,26 +14,31 @@ export class RegisterComponent implements OnInit {
     bloodGroups: Observable<any>;
     educationalQualifications: Observable<any>;
     humanRelations: Observable<any>;
+    languages: Observable<any>;
+    genders: Observable<any>;
+    martialStatus: Observable<any>;
+    religions: Observable<any>;
 
   registerForm = this.registerFormBuilder.group({
     personalDetails: this.registerFormBuilder.group({
       firstName: ['', Validators.required],
       lastName: ['', Validators.required],
-      middleName: ['', Validators.required],
+      middleName: [''],
       gender: ['', Validators.required],
       dob: ['', Validators.required],
       martialStatus: ['', Validators.required],
       diffAbled: ['', Validators.required],
       email1: ['', [Validators.required, Validators.email]],
-      email2: ['', [Validators.required,  Validators.email]],
-      mobile1: ['', [Validators.required, Validators.minLength(10)]],
-      mobile2: ['',  [Validators.required, Validators.minLength(10)]],
+      email2: ['', [Validators.email]],
+      mobile1: ['', [Validators.required, Validators.pattern('^[6-9]\\d{9}$')]],
+      mobile2: ['',  [Validators.pattern('^[6-9]\\d{9}$')]],
       religion: ['', Validators.required],
       nationality: ['', Validators.required],
       domicile: ['', Validators.required],
-      aadharNo: ['', Validators.required],
-      panNo: ['', Validators.required],
-      passportNo: ['', Validators.required]
+      aadharNo: ['', [Validators.required,Validators.pattern('^([0-9]){12}$')]],
+      panNo: ['', [Validators.pattern('^([a-zA-Z]){5}([0-9]){4}([a-zA-Z]){1}?$')]],
+      passportNo: ['', [Validators.pattern('^(?!^0+$)[a-zA-Z0-9]{3,20}$')]],
+      languagesKnown :['']
     }),
     address: this.registerFormBuilder.group({
       street: [''],
@@ -55,32 +60,19 @@ export class RegisterComponent implements OnInit {
           vehicleNo: ['']
       })
   });
-
-  genderList: Array<any> = [
-    {'value': 'M','text': 'Male'},
-    {'value': 'F','text': 'FeMale'},
-    {'value': 'O','text': 'Others'}
-  ];
-  martialStatus: Array<any> = [
-    {'value': true,'text': 'Married'},
-    {'value': false,'text': 'UnMarried'}
-  ];
-
-  religionList : Array<any> = [
-    {'value': 'Hindu', 'text': 'Hindu'},
-    {'value': 'Christian', 'text': 'Christian'},
-    {'value': 'Muslim', 'text': 'Muslim'},
-    {'value': 'Others', 'text': 'Others'}
-  ]
   
   constructor(private firestore: AngularFirestore,private registerFormBuilder: FormBuilder) {
     this.professions = this.firestore.collection('Professions',x=>x.orderBy('type')).valueChanges();
     this.bloodGroups = this.firestore.collection('BloodGroups',x=>x.orderBy('type')).valueChanges();
     this.educationalQualifications = this.firestore.collection('EducationalQualifications',x=>x.orderBy('degree')).valueChanges();
     this.humanRelations = this.firestore.collection('HumanRelations',x=>x.orderBy('relation')).valueChanges();
+    this.languages = this.firestore.collection('Languages',x=>x.orderBy('language')).valueChanges();
+    this.genders = this.firestore.collection('Genders',x=>x.orderBy('gender')).valueChanges();
+    this.martialStatus = this.firestore.collection('MaritalStatus',x=>x.orderBy('status')).valueChanges();
+    this.religions = this.firestore.collection('Religions',x=>x.orderBy('religion')).valueChanges();
    }
 
   ngOnInit() {
-  }
+  }  
 
 }
